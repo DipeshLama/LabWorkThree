48a1143623588ef894116e7062e25bd1fda0473f
DipeshLama <dipeslama32@gmail.com>
2020-12-29 13:53:14 +0545

    No scroll view

---
+++ app/src/main/java/com/dipesh/labworkthree/NoScrollViewActivity.kt
@@ -0,0 +1,11 @@
+package com.dipesh.labworkthree
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NoScrollViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_no_scroll_view)
+    }
+}
